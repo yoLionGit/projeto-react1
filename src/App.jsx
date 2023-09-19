bc4e0d47c16afcd6ec3192eb93a02432eff07ee5
yoLionGit <yoshiogamer@gmail.com>
2023-09-19 17:26:26 -0300

    Utilizando React fragments

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './index.css';
 import Frase from './components/Frase';
 import Pessoa from './components/Pessoa';
+import List from './components/list';
 
 function App() {
   const img = 'https://neofeed.com.br/wp-content/uploads/2019/10/ia-1.jpg';
@@ -9,13 +10,7 @@ function App() {
   return (
     <div className="App">
       <h1>Home</h1>
-      <Frase />
-      <Pessoa
-        nome="Exemplo1"
-        idade="99"
-        profissao="Programador"
-        foto="https://via.placeholder.com/150"
-      />
+      <List />
       <img src={img} alt="Imagem 1" />
     </div>
   );
---
+++ src/components/List.jsx
@@ -0,0 +1,15 @@
+import Item from './Item';
+
+function List() {
+  return (
+    <>
+      <h1>Minha Lista</h1>
+      <url>
+        <Item marca="Toyota" />
+        <Item marca="Mazda" />
+      </url>
+    </>
+  );
+}
+
+export default List;
